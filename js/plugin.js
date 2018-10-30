75410174abbd309fcdde420885b1c8985235e5e2
fremin33 <florian.dev33@gmail.com>
2018-10-30 12:56:07 +0100

    Initial commit

---
+++ js/plugin.js
@@ -0,0 +1,93 @@
+(function ($) {
+    $.fn.myPlugin = function (options) {
+        // Déclarations de valeurs par défaut
+        let settings = $.extend({
+            output: 'output',
+            dataDiv: 'data',
+            removeExtension: true,
+            loader: {
+                source: 'https://bit.ly/2Jmnf3u',
+                css: {
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    display: "none",
+                }
+            },
+            baseUrl: window.location.origin + window.location.pathname
+        }, options);
+
+        Loader.createLoader(settings.loader);
+        this.click(function (event) {
+            let link = $(this).attr("href").substring($(this).attr("href").lastIndexOf('/') + 1);
+            Page.getPage(settings, link);
+            ManageBrowser.updateHistory(link, settings)
+        });
+
+        window.onpopstate = function (event) {
+            let link = event.state ? event.state.pathFile : "/";
+            Page.getPage(settings, link);
+        };
+        return this;
+    };
+
+    $(document).ajaxSuccess(() => {
+        Loader.hideLoader();
+    });
+
+})(jQuery);
+
+
+
+
+// Gestion du navigateur (url + historique)
+let ManageBrowser = {
+    updateHistory: function (data, settings) {
+        let filename = data.substring(data.lastIndexOf('/') + 1);
+        let pathFile = (filename === "") ? settings.baseUrl : (settings.removeExtension) ? ManageBrowser.formatUrlWithoutExtension(filename) : filename
+        
+        history.pushState({
+            title: filename,
+            href: pathFile,
+            pathFile: filename
+        }, filename, pathFile);
+
+        ManageBrowser.updateTitle(filename);
+    },
+
+    updateTitle: function (filename) {
+        const title = ManageBrowser.formatUrlWithoutExtension(filename);
+        document.title = title.toUpperCase();
+    },
+
+    formatUrlWithoutExtension: function (link) {
+        return link.split('.').slice(0, -1).join('.')
+    },
+}
+
+
+// Gestion du chargement de la page
+let Page = {
+    getPage: function (settings, link) {
+        event.preventDefault();
+        $(`.${settings.output}`).load(`${settings.baseUrl}/${link} #${settings.dataDiv}`, () => {
+            Loader.showLoader();
+        });
+    }
+}
+
+
+// Gestion du loader
+let Loader = {
+    createLoader: function (loader) {
+        $('body').append(`<div class="loader"><img src="${loader.source}" /></div>`);
+        $('.loader').css(loader.css);
+    },
+    showLoader: function () {
+        $('.loader').show();
+    },
+    hideLoader: function () {
+        $('.loader').fadeOut();
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,13 @@
+Consignes :
+
+- Intégrer cette maquette en utilisant Flex.
+- Eviter le plus possible les utilisations des medias queries.
+- Ne pas modifier le HTML fourni SAUF pour les balises <picture> : Vous avez la liberté de choisir vos formats d'images
+- Fournir un rendu qui couvre toutes les résolutions, entre 960 et 480 pixels, mais aussi au dessus et en dessous.
+- A 960 et 480, être au plus proche des wire frame fournis.
+
+Objectif :
+
+- Appréhender flex et ses proprietés.
+- Travailler ses selecteurs CSS
+- Démontrer un bonne anticipation d'un contexte responsive avec peu de consignes.
